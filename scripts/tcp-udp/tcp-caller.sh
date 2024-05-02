19565c729dcecff2706bcdd363696fb6cee1f69d
Matteo Baiguini <mbaiguini@swissblock.net>
2024-05-02 15:38:44 +0200

    Updated tcp-caller script

--- scripts/tcp-udp/tcp-caller.sh
+++ scripts/tcp-udp/tcp-caller.sh
@@ -4,6 +4,9 @@
 echo "Start TCP caller to ${HOST}:${PORT} with message ${MSG}"
 while [ true ]
 do
+	# macos
+	# echo ${MSG} | nc -v ${HOST} ${PORT}
+	# linux
 	echo ${MSG} | nc -v -q 0 ${HOST} ${PORT}
 	sleep 1
 done
